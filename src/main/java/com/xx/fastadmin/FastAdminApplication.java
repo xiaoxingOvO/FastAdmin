65f3276efa9d4e2b41d35ad9d5a553ddd075103c
xiaoxing <793408755@qq.com>
2023-03-24 10:33:51 +0800

    init fastadmin

---
+++ src/main/java/com/xx/fastadmin/FastAdminApplication.java
@@ -0,0 +1,84 @@
+package com.xx.fastadmin;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+public class FastAdminApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(FastAdminApplication.class);
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext run = SpringApplication.run(FastAdminApplication.class, args);
+
+
+        log.info("\n" +
+                "////////////////////////////////////////////////////////////////////\n" +
+                "//                          _ooOoo_                               //\n" +
+                "//                         o8888888o                              //\n" +
+                "//                         88\" . \"88                              //\n" +
+                "//                         (| ^_^ |)                              //\n" +
+                "//                         O\\  =  /O                              //\n" +
+                "//                      ____/`---'\\____                           //\n" +
+                "//                    .'  \\\\|     |//  `.                         //\n" +
+                "//                   /  \\\\|||  :  |||//  \\                        //\n" +
+                "//                  /  _||||| -:- |||||-  \\                       //\n" +
+                "//                  |   | \\\\\\  -  /// |   |                       //\n" +
+                "//                  | \\_|  ''\\---/''  |   |                       //\n" +
+                "//                  \\  .-\\__  `-`  ___/-. /                       //\n" +
+                "//                ___`. .'  /--.--\\  `. . ___                     //\n" +
+                "//              .\"\" '<  `.___\\_<|>_/___.'  >'\"\".                  //\n" +
+                "//            | | :  `- \\`.;`\\ _ /`;.`/ - ` : | |                 //\n" +
+                "//            \\  \\ `-.   \\_ __\\ /__ _/   .-` /  /                 //\n" +
+                "//      ========`-.____`-.___\\_____/___.-`____.-'========         //\n" +
+                "//                           `=---='                              //\n" +
+                "//      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        //\n" +
+                "//             佛祖保佑       永不宕机      永无BUG                   //\n" +
+                "////////////////////////////////////////////////////////////////////");
+
+        ConfigurableEnvironment env = run.getEnvironment();
+        logApplicationStartup(env);
+    }
+
+    /**
+     * 打印服务地址
+     * @param env
+     */
+    private static void logApplicationStartup(Environment env) {
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        //服务端口
+        String serverPort = env.getProperty("server.port");
+        //服务名
+        String contextPath = StringUtils.isBlank(env.getProperty("server.servlet.context-path"))? "/":env.getProperty("server.servlet.context-path");
+
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\t{}://localhost:{}{}\n\t" +
+                        "External: \t{}://{}:{}{}\n\t" +
+                        "Swagger: \t{}://localhost:{}{}{}\n\t"+
+                        "----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol, serverPort, contextPath,
+                protocol, hostAddress, serverPort, contextPath,
+                protocol, serverPort,contextPath, "/doc.html");
+    }
+
+}
---
+++ src/test/java/com/xx/fastadmin/FastAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xx.fastadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
